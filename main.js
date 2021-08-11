2048602791013c2219695544a3645b51405d2804
Adamololol <86844711+Adamololol@users.noreply.github.com>
2021-08-11 12:52:54 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,69 @@
+var width = screen.width;
+var new_width = screen.width - 70;
+var heigth = screen.height;
+var new_heigth = screen.height - 300;
+if(width < 992)
+{
+    document.getElementById("myCanvas").width = new_width;
+    document.getElementById("myCanvas").height = new_heigth;
+    document.body.style.overflow = "hidden";
+}
+
+
+var last_position_of_x, last_position_of_y;
+
+    canvas = document.getElementById('myCanvas');
+    ctx = canvas.getContext("2d");
+    
+    color = "black";
+    width_of_line = 2;
+
+    canvas.addEventListener("touch_start", my_touch_start);
+    
+    function my_touch_start(e)
+    {
+     console.log(my_touch_start);
+
+       last_position_of_x = e.touches[0].clientX - canvas.offsetLeft
+       last_position_of_y = e.touches[0].clientY - canvas.offsetTop
+       ctx.beginPath();
+       ctx.strokeStyle = color;
+       ctx.lineWidth = width_of_line;
+
+       console.log("Last position of x and y coordinates = ");
+       console.log("x = " + last_position_of_x + "y = " + last_position_of_y);
+       ctx.moveTo(last_position_of_x, last_position_of_y);
+
+       console.log("Current position of x and y coordinates = ");
+       console.log("x  = " + current_position_of_touch_x + "y = " + current_position_of_touch_y);
+       ctx.lineTo(current_position_of_touch_x, current_position_of_touch_y);
+       ctx.stroke()
+       last_position_of_x = current_position_of_touch_x; 
+       last_position_of_y = current_position_of_touch_y;
+    }
+
+    canvas.addEventListener("touch_move", my_touch_move);
+    function my_touch_move(e)
+    {
+
+         current_position_of_mouse_x = e.clientX - canvas.offsetLeft;
+         current_position_of_mouse_y = e.clientY - canvas.offsetTop;
+
+        ctx.beginPath();
+        ctx.strokeStyle = color;
+        ctx.lineWidth = width_of_line;
+
+        console.log("Last position of x and y coordinates = ");
+        console.log("x = " + last_position_of_x + "y = " + last_position_of_y);
+        ctx.moveTo(last_position_of_x, last_position_of_y);
+
+        console.log("Current position of x and y coordinates = ");
+        console.log("x  = " + current_position_of_mouse_x + "y = " + current_position_of_mouse_y);
+        ctx.lineTo(current_position_of_mouse_x, current_position_of_mouse_y);
+        ctx.stroke();
+      
+
+        last_position_of_x = current_position_of_mouse_x; 
+        last_position_of_y = current_position_of_mouse_y;
+    
+    }
